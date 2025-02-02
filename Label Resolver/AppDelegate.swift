c6f4c19d46adffcb3beacb195b79af5d32f5c475
Gil Burns <gil@gilburns.com>
2025-02-02 09:44:11 -0600

    Update AppDelegate.swift

--- Label Resolver/AppDelegate.swift
+++ Label Resolver/AppDelegate.swift
@@ -22,5 +22,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return true
     }
 
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
 }
 
